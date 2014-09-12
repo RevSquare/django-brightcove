8be1538963de2615aac6e8b105718c788f8f82f5
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2014-09-11 17:10:01 -1000

    momentarily disabling the refresh

--- django_brightcove/views.py
+++ django_brightcove/views.py
@@ -1,11 +1,8 @@
 from django.http import HttpResponse, Http404
 
-from .widgets import BrightcoveNumberInput
-
 
 def refresh_item_list(request, pk):
     """Updates the select of brightcove items in a form"""
     if not request.is_ajax():
         raise Http404
-    widget = BrightcoveNumberInput()
     return HttpResponse(pk)
